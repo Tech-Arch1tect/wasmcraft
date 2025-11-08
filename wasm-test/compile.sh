45934e67dcea9874ed7b580071835a67d4e5ffe9
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-11-08 01:03:42 +0000

    wasm: add monitor example to compile.sh

--- wasm-test/compile.sh
+++ wasm-test/compile.sh
@@ -6,6 +6,11 @@ cd redstone
 tinygo build -o ../redstone.wasm -target=wasi .
 cd ..
 
+cd monitor_one
+tinygo build -o ../monitor_one.wasm -target=wasi .
+cd ..
+
+echo ""
 echo "Done! Files compiled:"
 ls -lh *.wasm
 
@@ -14,3 +19,8 @@ echo "To test in Minecraft:"
 echo "1. Start HTTP server: python3 -m http.server 8000"
 echo "2. In game: download http://localhost:8000/redstone.wasm"
 echo "3. In game: run redstone"
+echo ""
+echo "For monitor example:"
+echo "1. Place a monitor block and set label to 'monitor_test'"
+echo "2. In game: download http://localhost:8000/monitor_one.wasm"
+echo "3. In game: run monitor_one"
