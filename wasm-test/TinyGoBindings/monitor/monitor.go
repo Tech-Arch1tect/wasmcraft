1ef991024ff4826a8ff0ac2420af2009e15bbd40
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-11-09 02:31:40 +0000

    TinyGoBindings: add monitor text functions

--- wasm-test/TinyGoBindings/monitor/monitor.go
+++ wasm-test/TinyGoBindings/monitor/monitor.go
@@ -29,6 +29,15 @@ func monitorDrawVLine(idPtr, idLen, x, y, length, r, g, b uint32)
 //go:wasmimport env monitor_draw_rect
 func monitorDrawRect(idPtr, idLen, x, y, width, height, r, g, b uint32)
 
+//go:wasmimport env monitor_draw_char
+func monitorDrawChar(idPtr, idLen, x, y, c, fgR, fgG, fgB, bgR, bgG, bgB, scale uint32)
+
+//go:wasmimport env monitor_draw_text
+func monitorDrawText(idPtr, idLen, x, y, textPtr, textLen, fgR, fgG, fgB, bgR, bgG, bgB, scale uint32) uint32
+
+//go:wasmimport env monitor_measure_text
+func monitorMeasureText(idPtr, idLen, textPtr, textLen, scale uint32) uint32
+
 func SetPixel(monitorID string, x, y, r, g, b int) {
 	idBytes := []byte(monitorID)
 	idPtr := uint32(uintptr(unsafe.Pointer(&idBytes[0])))
@@ -110,6 +119,44 @@ func DrawRect(monitorID string, x, y, width, height, r, g, b int) {
 	idLen := uint32(len(idBytes))
 
 	monitorDrawRect(idPtr, idLen, uint32(x), uint32(y), uint32(width), uint32(height), uint32(r), uint32(g), uint32(b))
+}
+
+func DrawChar(monitorID string, x, y int, c rune, fgR, fgG, fgB, bgR, bgG, bgB, scale int) {
+	idBytes := []byte(monitorID)
+	idPtr := uint32(uintptr(unsafe.Pointer(&idBytes[0])))
+	idLen := uint32(len(idBytes))
+
+	monitorDrawChar(idPtr, idLen, uint32(x), uint32(y), uint32(c), uint32(fgR), uint32(fgG), uint32(fgB), uint32(bgR), uint32(bgG), uint32(bgB), uint32(scale))
+}
+
+func DrawText(monitorID string, x, y int, text string, fgR, fgG, fgB, bgR, bgG, bgB, scale int) int {
+	idBytes := []byte(monitorID)
+	idPtr := uint32(uintptr(unsafe.Pointer(&idBytes[0])))
+	idLen := uint32(len(idBytes))
+
+	textBytes := []byte(text)
+	textPtr := uint32(uintptr(unsafe.Pointer(&textBytes[0])))
+	textLen := uint32(len(textBytes))
+
+	width := monitorDrawText(idPtr, idLen, uint32(x), uint32(y), textPtr, textLen, uint32(fgR), uint32(fgG), uint32(fgB), uint32(bgR), uint32(bgG), uint32(bgB), uint32(scale))
+	return int(width)
+}
+
+func MeasureText(monitorID string, text string, scale int) (width, height int) {
+	idBytes := []byte(monitorID)
+	idPtr := uint32(uintptr(unsafe.Pointer(&idBytes[0])))
+	idLen := uint32(len(idBytes))
+
+	textBytes := []byte(text)
+	textPtr := uint32(uintptr(unsafe.Pointer(&textBytes[0])))
+	textLen := uint32(len(textBytes))
+
+	resultPtr := monitorMeasureText(idPtr, idLen, textPtr, textLen, uint32(scale))
+
+	widthVal := *(*int32)(unsafe.Pointer(uintptr(resultPtr)))
+	heightVal := *(*int32)(unsafe.Pointer(uintptr(resultPtr + 4)))
+
+	return int(widthVal), int(heightVal)
 }
 
 type Color struct {
