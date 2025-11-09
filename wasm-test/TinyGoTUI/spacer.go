4187b6209a745b6f3ce1262a905f147d1411ad66
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-11-09 13:43:33 +0000

    Tui: add spacer component

---
+++ wasm-test/TinyGoTUI/spacer.go
@@ -0,0 +1,34 @@
+package tui
+
+type Spacer struct {
+	Width  int
+	Height int
+}
+
+func NewSpacer(width, height int) *Spacer {
+	return &Spacer{
+		Width:  width,
+		Height: height,
+	}
+}
+
+func NewHSpacer(width int) *Spacer {
+	return &Spacer{
+		Width:  width,
+		Height: 0,
+	}
+}
+
+func NewVSpacer(height int) *Spacer {
+	return &Spacer{
+		Width:  0,
+		Height: height,
+	}
+}
+
+func (s *Spacer) MinSize(monitorID string) (width, height int) {
+	return s.Width, s.Height
+}
+
+func (s *Spacer) Render(monitorID string, region Rect) {
+}
